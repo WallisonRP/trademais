c6e365eb3e28a9877b63fb00556367e225f243b7
WallisonRP <wallison0076@gmail.com>
2023-02-02 11:32:22 -0300

    reajuste menu

--- src/components/IconButton/indes.tsx
+++ src/components/IconButton/indes.tsx
@@ -2,8 +2,10 @@ import whatsappIcon from "../../assets/icons/whatsapp-icon.svg";
 
 export function IconButton() {
   return (
-    <button className="py-2 px-3 flex bg-[#FFBD08] text-black rounded-3xl font-audiowide text-xl">
-      <span className="mr-2 lg:text-base md:text-xs">Whatsapp</span>
+    <button className="md:py-2 md:px-3 px-3 py-3 flex bg-[#FFBD08] text-black rounded-3xl font-audiowide text-xl">
+      <span className="mr-2 lg:text-base md:text-xs md:block hidden">
+        Whatsapp
+      </span>
       <img className="lg:w-6 md:w-4" src={whatsappIcon} alt="" />
     </button>
   );
